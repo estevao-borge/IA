d94462e472e0ddc2f44bcefc1035bc7515ed7aab
estevao-borge <72615275+estevao-borge@users.noreply.github.com>
2020-10-13 20:22:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,14 +9,16 @@ public class Main
 		
 		BestFirst s = new BestFirst();
 		Iterator<BestFirst.State> it = s.solve(new Board(sc.next()), new Board(sc.next()));
-		if (it == null) System.out.println("no solution was found");
+		if (it == null) 
+			System.out.println("no solution was found");
 		else 
 		{
 			while(it.hasNext()) 
 			{
 				BestFirst.State i = it.next(); 
 				System.out.println(i);
-				if (!it.hasNext()) System.out.println(i.getG());
+				if (!it.hasNext()) 
+					System.out.println(i.getG());
 			}
 		}
 		sc.close();
