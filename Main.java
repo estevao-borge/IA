b57e36cf2dfa4315f4c58ff63bb2c5225c692810
estevao-borge <72615275+estevao-borge@users.noreply.github.com>
2020-10-16 21:30:35 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main
 	{
 		Scanner sc = new Scanner(System.in);
 		
-		BestFirst s = new BestFirst();
+		/*BestFirst s = new BestFirst();
 		Iterator<BestFirst.State> it = s.solve(new Board(sc.next()), new Board(sc.next()));
 		if (it == null) 
 			System.out.println("no solution was found");
@@ -20,7 +20,12 @@ public class Main
 				if (!it.hasNext()) 
 					System.out.println(i.getG());
 			}
-		}
+		}*/
+		
+		Board x = new Board(sc.next());
+		System.out.println(x.toString());
+		x.children();
+		
 		sc.close();
 	}
 }
